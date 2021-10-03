e7545e220121b048f424d4b5cebb8ae2a6dda6c9
Franco Valdes <franco@union.co>
2021-10-03 16:48:21 -0400

    use custom domain instead of store name

--- src/utils/shopify.ts
+++ src/utils/shopify.ts
@@ -2,7 +2,8 @@ import Client from 'shopify-buy';
 
 // Otherwise, setup the client and export
 const options = {
-  domain: `${process.env.NEXT_PUBLIC_STORE_ID}.myshopify.com`,
+  // domain: `${process.env.NEXT_PUBLIC_STORE_ID}.myshopify.com`,
+  domain: `${process.env.NEXT_PUBLIC_CUSTOM_DOMAIN}`,
   storefrontAccessToken: process.env.NEXT_PUBLIC_STOREFRONT_TOKEN!,
 }
 
