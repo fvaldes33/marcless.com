18c4cd57548629785e94748894acd98f06b0aa63
Franco Valdes <franco@union.co>
2022-02-18 23:09:28 -0500

    redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/products/:path*',
+        destination: '/shop/:path*',
+        permanent: true,
+      }
+    ];
+  },
   async rewrites() {
     return [
       {
